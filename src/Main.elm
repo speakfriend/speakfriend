2e2d1666923d2749fb079505476be325c5cd695b
teesloane <tylersloane@gmail.com>
2017-05-14 14:21:52 -0400

    Begin styling. Created editor config. Cleanup.

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,59 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 main =
-    text "Speak Friend And Enter"
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , email : String
+    }
+
+
+model : Model
+model =
+    Model "" ""
+
+
+
+-- Update
+
+
+type Msg
+    = Name String
+    | Email String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+        Email email ->
+            { model | email = email }
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "SpeakFriend" ]
+        [ header
+            [ class "Speak__Header" ]
+            [ text "Speak Friend" ]
+        , section
+            [ class "Speak__form" ]
+            [ text "Form - To do." ]
+        ]
